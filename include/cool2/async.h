05447c7eb283c5c5bc4429da16b9505862fa3939
Leon Mlakar <leon@digiverse.si>
2016-09-21 10:22:34 +0200

    work in progress: documenting new task API

--- include/cool2/async.h
+++ include/cool2/async.h
@@ -192,6 +192,131 @@ class runner
   std::shared_ptr<impl::runner> m_impl;
 };
 
+/**
+ * A class template representing the basic unit that can be scheduled for execution
+ * by one of the @ref runner "runners".
+ *
+ * A task can be one of the following:
+ *  - a <em>simple task</em>, which contains a @em Callable and is associated with a
+ *    specific @ref runner. Invoking the run method on a simple task will schedule
+ *    the task for execution with the associated @ref runner by insertion its @em Callable
+ *    into the runner's task queue. Simple task objects are created via
+ *    taskop::create method teplate.
+ *  - a <em>compound task</em> which contains other tasks as its subtasks. Subtasks can be
+ *    either compound or simple tasks. When a compound task gets activated (via
+ *    a call to run(), for instance) it fetches one or more of its simple subtasks 
+ *    and schedules them for execution. The compound task remains active until the 
+ *    execution of all of its subtasks is complete. Since the compound tasks do not
+ *    contain executable code they are not associated with any @ref runner. Componud
+ *    tasks are created by any of the following @ref taskop methods templates: @ref taskop::parallel
+ *    "parallel()", @ref taskop::sequential "sequential()", @ref taskop::intercept
+ *    "intercept()", or @ref taskop::intercept_all "intercept_all()", or via the corresponding
+ *    task class template member methods templates.
+ *  - an <em>exception handling task</em> which intercepts an exception of a particular
+ *    type, or any exception, that may be thrown by the preceding task. An exception
+ *    handling task is any compound or simple tasks that adheres to certain limitations
+ *    about its parameter or return value type. One or more exception handling tasks
+ *    can be attached to any task using  @ref taskop method templates @ref taskop::intercept
+ *    "intercept()" or @ref taskop::intercept_all() "intercept_all()", or by using
+ *    the corresponding method templates of the task class template. Note that the
+ *    exception handling tasks are normally not sheduled for execution; they get
+ *    scheduled only if an eception was thrown in one of the preceding tasks
+ *    and is caught by the exception handling task.
+ *
+ * The compound tasks are internally organized in one of two possible ways:
+ *  - @em parallel organization; when such compund task is activated it will
+ *    immediatelly schedule all of its subtasks for execution with their respective
+ *    @ref runner "runners". Note that should two or more subtasks be associated
+ *    with the the same @ref runner, they will still get scheduled immediatelly but
+ *    the scheduling order in which they are passed to the @ref runner is not
+ *    defined. The parallel task remains active until the
+ *    last of its subtasks completes the execution. Then it collects the results
+ *    of all subtasks, if any, and returns the collection as its result.
+ *    Parallel compound tasks are created by using @ref taskop
+ *    method template @ref taskop::parallel "parallel()" or the corresponding method
+ *    template of the task class template.
+ *
+ *  - @em sequential organization; when such compound tasks is activated, only
+ *    the first task in the sequence is scheduled for execution; when this task
+ *    task is completed, its result, if any, is passed as an input parameter to
+ *    the next task in the sequence, which is then scheduled for execution with
+ *    its associated runner, and so on. The sequential compound tasks remains
+ *    activa until the last task in the sequence completes its execution, when
+ *    it collects the result of the last task, if any, and returns it as its
+ *    own result.  Sequential compound tasks are created via a call to @ref taskop
+ *    method template @ref taskop::sequential "sequential()" or the corresponding method
+ *    template of the task class template.
+ * 
+ * Every task either no input parameters or exactly one input parameter and a 
+ * result type, which can be @c void if the task produces no results. For simple
+ * tasks the presence or absence of the input parameter and its type, as well as
+ * the task's result type (or @c void if none) are deduced from its @em Callable 
+ * passed to the @ref taskop::create method template.
+ *
+ * The presence of the <em>input parameter</em> and its type of the compound task are
+ * deduced from the first task in the parameter list passed to either
+ * taskop:: sequential or taskop::parallel method. Thus by definition the
+ * the input paramter presence and its type of the sequential compound task
+ * matches the input parameter of the first task in the sequence, and when the
+ * sequential compound task is activated, it will pass its input parameter, if
+ * any, to the first task in the sequence. If the compound task is a parallel task,
+ * all of its other subtasks must match the input parameter of the first task
+ * passed to the taskop::parallel method template, and when parallel compound task is
+ * activated it will pass its input parameter to all of its subtasks.
+ *
+ * The <em>return value</em> type of the sequential compound task is deduced from the
+ * last subtask in the sequence (last task parameter passed to taskop::sequential).
+ * The return value type of the parallel compound task is defined to be
+ * <tt>std::tuple<t1, t2, t3 ...></tt> where @c t1, @c t2, @c t3 ... are the
+ * result types of its subtasks in the exact order as passed to taskop::parallel.
+ * If a subtask of the parallel compound task does not return result
+ * (result type @c void), a type <tt>void*</tt> is used in its place in the
+ * <tt>std::tuple</tt> template parameter list. This element of the tuple is
+ * never set, it is just used as a placeholder to enable the instatination of the
+ * result tuple.
+ *
+ * The <em>exception handling tasks</em> that are to intercept and handle exceptions
+ * of a particular type must accept the parameter of this type (or better, a
+ * const reference to avoid object slicing). The exception handling tasks that
+ * are to intercept and handle any exception must accept <tt>std::exception_ptr</tt> as their
+ * parameter. The result type of the exception handling task must match the
+ * the result type of the task to which they are attached (preceding task).
+ *
+ * <b>Handling of Exceptions</b>
+ *
+ * The way the task compositions (compount tasks) handle the exceptions has been
+ * modeled to follow the C++ @c catch paradigm as close as possible, having in
+ * mind that the simple tasks exectute their @em Callables asynchronously. This
+ * means that an exception thrown in one @em Callable cannot be simply caught
+ * in another @em Callable since this another  @em Callable may execute some time
+ * in the future or even has already completed its execution. This is where the
+ * <em>exception handling</em> taks enter the picture. When a currently executing 
+ * @em Callable throws a C++ exception, which remains unhandled when the execution
+ * of the throwing callable completes, the task enclosing the @em Callable that
+ * threw will intercept the exception and pass it on to the next task in the
+ * sequence. If task is not designated to handle this exception type it will
+ * pass it on to the next task, and so on, until the exception reaches the
+ * eception handling task that will accept this, or any, exception type. If
+ * no matching exception handling task is found the exception will be silently
+ * and irrevocably discarded. The following is the full set of rules that govern
+ * the handling of exceptions:
+ *   - an exception thrown in a task which is not part of the @em sequential 
+ *     compound task, and is not either caught or intercepted by this task, is 
+ *     irrevocably lost
+ *   - an exception thrown inside a subtask of the @em sequential task, and not
+ *     intercepted within this subtask, will travel along the sequence until it
+ *     reaches an appropriate <em>exception handling</em> subtask. If no appropriate
+ *     exeption handling subtask is reached before the end of the sequence the
+ *     exception will exit the compound task and will be passed on to the next
+ *     task in the sequence, if any. <em>None of the tasks in sequence between 
+ *     the task that threw the exception and the task that intercepted it, or
+ *     the end of the sequence, will be scheduled for execution</em>.
+ *   - an exception thrown inside a subtask of the @em parallel task will
+ *     affect this subtask only. The remaining subtasks of the parallel compound
+ *     task will run to their completion. Then the compound task will pass the
+ *     exception on to the next task in the sequence, if any.
+ *
+ */
 template <typename ResultT, typename ParamT>
 class task
 {
@@ -204,20 +329,156 @@ class task
   template <typename FunctionT>
   task(const runner::weak_ptr_t& r_, FunctionT&& f_)
     : m_runner(r_)
-  {
+  { }
 
-  }
+  /**
+   * Create a compound task which will concurrently execute its subtasks.
+   *
+   * Using this method in the following code fragment:
+   * @code
+   *   auto new_task = task.parallel(task1, task2, task3);
+   * @endcode
+   * is exactly the same as using:
+   * @code
+   *   auto new_task = taskop::parallel(task, task1, task2, task3);
+   * @endcode
+   * This method is provided for convenience to allow chaining the
+   * task operations such as:
+   * @code
+   *   task.parallel(task1, task2).intercept(handler).run();
+   * @endcode
+   * @warning This method invalidates @c this task object.
+   * @see taskop::parallel
+   * @see @ref sequential
+   * @see taskop::sequential
+   */
+  template <typename... TaskT>
+  task<typename impl::traits::parallel_result<result_t, TaskT...>::type
+     , parameter_t>
+  parallel(TaskT&&... tasks);
+
+  /**
+   * Create a compound task which will cexecute its subtasks one after another.
+   *
+   * Using this method in the following code fragment:
+   * @code
+   *   auto new_task = task.sequential(task1, task2, task3);
+   * @endcode
+   * is exactly the same as using:
+   * @code
+   *   auto new_task = taskop::sequential(task, task1, task2, task3);
+   * @endcode
+   * This method is provided for convenience to allow chaining the
+   * task operations such as:
+   * @code
+   *   task.sequential(task1, task2).intercept(handler).run();
+   * @endcode
+   * @warning This method invalidates @c this task object.
+   * @see taskop::parallel
+   * @see @ref parallel
+   * @see taskop::sequential
+   */
+  template <typename... TaskT>
+  task<typename impl::traits::sequence_result<result_t, TaskT...>::type
+     , parameter_t>
+  sequential(TaskT&&... tasks);
+
+  /**
+   * Add an exception handler(s) for specific exception(s) to the task.
+   *
+   * Using this method in the following code fragment:
+   * @code
+   *   auto new _task = task.intercept(handler1, handler2);
+   * @endcode
+   * is exactly the same as using:
+   * @code
+   *   auto new _task = taskop::intercept(task, handler1, handler2);
+   * @endcode
+   * This method is provided for convenience to allow chaining the
+   * task operations, such as:
+   * @code
+   *   task.intercept(handler).parallel(task2).run();
+   * @endcode
+   * @warning This method invalidates @c this task object.
+   * @see taskop::intercept
+   * @see @ref intercept_all
+   * @see taskop::intercept_all
+   */
+  template <typename... HandlerTaskT>
+  task intercept(HandlerTaskT&&... handlers);
+
+  /**
+   * Add an exception handler for any exception to the task.
+   *
+   * Using this method in the following code fragment:
+   * @code
+   *   auto new _task = task.intercept_all(handler);
+   * @endcode
+   * is exactly the same as using:
+   * @code
+   *   auto new _task = taskop::intercept_all(task, handler);
+   * @endcode
+   * This method is provided for convenience to allow chaining the
+   * task operations, such as:
+   * @code
+   *   task.intercept_all(handler).parallel(task2).run();
+   * @endcode
+   * @warning This method invalidates @c this task object.
+   * @see taskop::intercept
+   * @see @ref intercept
+   * @see taskop::intercept_all
+   */
+  template <typename HandlerTaskT>
+  task intercept_all(HandlerTaskT&& handler);
+
+  /**
+   * Schedule a task for execution by its runner.
+   *
+   * If the @ref task is a simple task, this method will sumbit its
+   * @em Callable to its associated @ref runner for execution. If the task
+   * is a compound task, it will activate it. The activation means that the
+   * compound task will start submitting its subtasks to their respective
+   * @ref runner "runners" for execution.
+   *
+   * @param param_ the parameater forwarded to the task.
+   */
+  void run(/*parameter_t&& param_*/)
+  { }
+
   task() { }
  private:
   runner::weak_ptr_t m_runner;
 };
 
-
+/**
+ * Class implementing the operations on tasks.
+ */
 class taskop
 {
  private:
 
  public:
+  /**
+   * Create a simple task.
+   *
+   * Creates and returns a simple task associated with the @ref runner. When
+   * run via @ref task::run() method, the task will submit its @em Callable for
+   * execution with the associated runner. The @em Callable must satisfy the
+   * following requirements:
+   *   - it must accept <tt>const std::weak_ptr<runner>&</tt> as its first
+   *     parameter
+   *   - it may accept one additional input parameter which is then the input
+   *     parameter of the task
+   *   - it may, but does not need to, return a value of any type.
+   *   - it can be a function pointer, lambda closure, @c std::function, or
+   *     functor object as long as it provides only a single overload of the
+   *     function call operator <tt>()</tt>.
+   *
+   * @note Unfortunately the functors returned by the @c std::bind template
+   *   provide multiple function call operator overloads and cannot be passed
+   *   to the simple task directly, without transforming them into @c std::function
+   *   object first.
+   */
   template <typename CallableT>
   static task<typename impl::traits::function_traits<CallableT>::result_type
             , typename impl::traits::arg_type<1, CallableT>::type>
@@ -265,6 +526,9 @@ class taskop
           >();
   }
 
+  /**
+   * Create a compound task which will execute its subtasks one after another.
+   */
   template <typename... TaskT>
   static task<typename impl::traits::sequence_result<TaskT...>::type
             , typename impl::traits::first_task<TaskT...>::type::parameter_t>
@@ -277,30 +541,72 @@ class taskop
               , typename impl::traits::first_task<TaskT...>::type::parameter_t
           >();
   }
-};
 
-
-#if 0
-template <typename ResultT, typename... ResultY>
-class sequential
-{
-};
-
-template <typename ResultT, typename... ResultY>
-class sequential<task<ResultT>, ResultY...>
-{
-
-};
-
-template <typename ResultT, typename ResultY>
-class sequential<task<ResultT>, task<ResultY>>
-{
- public:
-  static task<ResultY> combine(const task<ResultT>&lhs, const task<ResultY>& rhs)
+  /**
+   * Add am exception handler(s) to the task.
+   */
+  template <typename TaskT, typename... HandlerTaskT>
+  static TaskT intercept(TaskT&& task, HandlerTaskT&&... handlers)
   {
-    return task<ResultY>();
+    static_assert(
+        impl::traits::all_same<typename TaskT::result_t, typename HandlerTaskT::result_t...>::value
+      , "The task and its exception handlers must have the same result type");
+    return TaskT();
+  }
+  /**
+   * Add an exception handler to the task.
+   */
+  template <typename TaskT, typename HandlerTaskT>
+  static TaskT intercept_all(TaskT&& task, HandlerTaskT&& handler)
+  {
+    static_assert(
+        std::is_same<typename TaskT::result_t, typename HandlerTaskT::result_t>::value
+      , "The task and its exception handlers must have the same result type");
+    static_assert(
+        std::is_same<std::exception_ptr, typename std::decay<typename HandlerTaskT::parameter_t>::type>::value
+      , "The exception handler to catch all exceptions must accept std::exception_ptr as its parameter");
+    return TaskT();
   }
 };
-#endif
+
+// ---------------------------------------------------------------------------
+// implementation of task methods
+
+template <typename ResultT, typename ParameterT>
+template <typename... HandlerTaskT>
+inline task<ResultT, ParameterT>
+task<ResultT, ParameterT>::intercept(HandlerTaskT&&... handlers)
+{
+  return taskop::intercept(*this, std::forward<HandlerTaskT>(handlers)...);
+}
+
+template <typename ResultT, typename ParameterT>
+template <typename HandlerTaskT>
+inline task<ResultT, ParameterT>
+task<ResultT, ParameterT>::intercept_all(HandlerTaskT&& handler)
+{
+  return taskop::intercept_all(*this, std::forward<HandlerTaskT>(handler));
+}
+
+template<typename ResultT, typename ParameterT>
+template <typename... TaskT>
+inline task<typename impl::traits::parallel_result<ResultT, TaskT...>::type
+          , ParameterT>
+task<ResultT, ParameterT>::parallel(TaskT&&... tasks)
+{
+  return taskop::parallel(*this, std::forward<TaskT>(tasks)...);
+}
+
+template<typename ResultT, typename ParameterT>
+template <typename... TaskT>
+inline task<typename impl::traits::sequence_result<ResultT, TaskT...>::type
+          , ParameterT>
+task<ResultT, ParameterT>::sequential(TaskT&&... tasks)
+{
+  return taskop::sequential(*this, std::forward<TaskT>(tasks)...);
+}
+  
+
+
 } } // namespace
 #endif
